3de722f3e45c3951634f7d3cc7eca68a0c21fb25
Juan Serna <jsernaq02@gmail.com>
2025-03-14 13:24:40 -0500

    Initial settings

---
+++ src/app/features/sales/sales.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  templateUrl: './sales.page.html',
+  styleUrls: ['./sales.page.scss'],
+  standalone: false
+})
+export class SalesPage  {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
